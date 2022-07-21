a2c4a4790b1c4faca786cb6b5891eec2cc9207ce
Juliana <omoreiraju@gmail.com>
2022-07-20 22:27:56 -0300

    URL

--- ProjetoSenacStore/urls.py
+++ ProjetoSenacStore/urls.py
@@ -21,5 +21,5 @@ from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('store.urls'))
+    path('', include('Store.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
